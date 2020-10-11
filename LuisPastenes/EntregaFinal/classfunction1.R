ca8270e8095044b0c400a37a11a485da740a90b0
nxzhox <46355067+nxzhox@users.noreply.github.com>
2020-10-11 09:37:49 -0300

    Update classfunction1.R

--- LuisPastenes/EntregaFinal/classfunction1.R
+++ LuisPastenes/EntregaFinal/classfunction1.R
@@ -6,7 +6,9 @@
 
 classfunction<-function(datos=x)
 {
-  #Funcion MEAN SD
+  
+  #Funcion MEAN SD encargada de generar datos aleatorios con una media y desviacion estandar dada.
+
   meanSD <- function(x, mean, sd) {
     
    
@@ -47,26 +49,27 @@ classfunction<-function(datos=x)
   
   
   superdata<-list()
-  
+  #genero variables segun la cantidad de sources ingresadas.
   var_names <- paste("dNb", 1:datos$n_sources, sep="")
   var_names2 <- paste("dCb", 1:datos$n_sources, sep="")
   var_names3 <- paste("deltaNb", 1:datos$n_sources, sep="")
   var_names4 <- paste("deltaCb", 1:datos$n_sources, sep="")
   for (i in 1:datos$n_sources)
   {
+    #asignacion a dNb
     superdata[var_names[i]]<-list(meanSD(25,datos$source_means[i,2],datos$source_sds[i,2]))
-    
+     #asignacion a dCb
     superdata[var_names2[i]]<-list(meanSD(25,datos$source_means[i,1],datos$source_sds[i,1]))
-    
+     #asignacion a deltaNb
     superdata[var_names3[i]]<-list(meanSD(25,datos$correction_means[i,2],datos$correction_sds[i,2]))
-    
+     #asignacion a deltaCb
     superdata[var_names4[i]]<-list(meanSD(25,datos$correction_means[i,1],datos$correction_sds[i,1]))
     
   }
-  
+  #asignacion datos del consumidor
   superdata$dNc<-datos$mixtures[,2]
   superdata$dCc<-datos$mixtures[,1]
-  
+  #atributos 
   attr(superdata,"class")<-"isotopeData"
   attr(superdata,"consumer")<-"consumer"
   
